dc7bb7858badca0a03cfffb953744fcf2a287698
Firmin Martin <firminmartin24@gmail.com>
2021-06-24 12:21:20 +0200

    Add a new parse engine for the CNTRL.

---
+++ org-dict-cntrl.el
@@ -0,0 +1,38 @@
+;;; org-dict-cntrl.el --- Dictionary parse engine of the CNTRL -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Firmin Martin
+
+;; Author: Firmin Martin
+;; Maintainer: Firmin Martin
+;; Version: 0.1
+;; Keywords: convenience, dictionary
+;; URL: https://www.github.com/firmart/org-dict
+;; Package-Requires: ((emacs "25.1") (org "9.3") (cl-lib "0.5"))
+
+
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of
+;; the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file provides a dictionary parse engine for the French dictionaries available
+;; on CNTRL (https://cnrtl.fr/definition/).
+
+;;; Code:
+;;; Custom group
+;;;; General settings
+;;; Internal variables
+;;; Internal functions
+;;; Interactive functions
+;;; org-dict.el ends here
+(provide 'org-dict-cntrl)
